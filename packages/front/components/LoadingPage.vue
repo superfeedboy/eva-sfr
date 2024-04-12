a22320d98d104ef3bf2879dd307139615a934b58
Gilles COGNIN <gillescognin@gmail.com>
2024-04-12 09:04:52 +0200

    Feat: Ajout d'une page intermédiaire de chargement de données

---
+++ packages/front/components/LoadingPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex w-full justify-center">
+    <img
+      src="/images/eva-sfr-logo.png"
+      alt="logo"
+      class="w-64 h-64 p-0 hover:drop-shadow-2xl animate-pulse"
+    />
+  </div>
+  <BubbleLoader class="text-EVAPurple justify-center" />
+  <div class="text-center text-xl">
+    <h1 v-if="info">{{ info }}</h1>
+    <h1 v-else>Chargement...</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps({
+  info: String,
+});
+
+const { info } = toRefs(props);
+</script>
