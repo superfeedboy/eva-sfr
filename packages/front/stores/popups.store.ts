5060a9d06def33b85706085bc8cba39016ec7e4c
Gilles COGNIN <gillescognin@gmail.com>
2024-04-12 09:03:42 +0200

    Feat: Création d'un store pour gérer les notifications de manière globale

---
+++ packages/front/stores/popups.store.ts
@@ -0,0 +1,48 @@
+import { AxiosError } from "axios";
+import { defineStore } from "pinia";
+
+import type { CustomError } from "~/types/CustomError.dt";
+import type { PopUp } from "~/types/CustomPopUp.dt";
+
+export const usePopUpsStore = defineStore("popups", () => {
+  const popup: Ref<Array<PopUp>> = ref([]);
+
+  const normalizeError = (error: Error | AxiosError | CustomError): PopUp => {
+    const timestamp = new Date().getTime();
+    if (error instanceof AxiosError) {
+      return {
+        title: `${error.response?.status || 500}: ${error.name}`,
+        message: error.message,
+        type: "error",
+        timestamp: timestamp,
+      };
+    }
+
+    return {
+      title: `500: ${error.name}`,
+      message: error.message,
+      type: "error",
+      timestamp: timestamp,
+    };
+  };
+
+  const setPopUp = (
+    datas: Error | CustomError | PopUp,
+    dataType: string
+  ): void => {
+    if (dataType === "error") {
+      popup.value.push(normalizeError(datas as Error | AxiosError));
+    } else {
+      popup.value.push({
+        ...(datas as PopUp),
+        timestamp: new Date().getTime(),
+      });
+    }
+  };
+
+  const removePopUp = (index: number): void => {
+    popup.value.splice(index, 1);
+  };
+
+  return { popup, setPopUp, removePopUp };
+});
