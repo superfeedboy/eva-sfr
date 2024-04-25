9e7469990346b7f4c45375e252f2ab45cd4a6196
Gilles COGNIN <gillescognin@gmail.com>
2024-04-25 18:19:25 +0200

    Tech: Mise à jour du middleware Axios

--- packages/front/api/axios.middleware.ts
+++ packages/front/api/axios.middleware.ts
@@ -1,16 +1,16 @@
-import axiosLib from "axios";
+import axiosLib, { type AxiosError } from "axios";
 import { version } from "../package.json";
-import { queries } from "./queries";
+import { gqlQuery } from "./queries";
 
 import type {
-  AxiosError,
   AxiosInstance,
   AxiosResponse,
   InternalAxiosRequestConfig,
 } from "axios";
+import type { GraphqlResult, GraphqlVariables } from "~/types/Query.dt";
 
 interface ExtendedAxiosInstance extends AxiosInstance {
-  graphql?: (query: string, variables?: Record<string, any>) => Promise<any>;
+  graphql?: <T>(query: string, variables?: GraphqlVariables) => Promise<T>;
 }
 
 const { VITE_API_URL, VITE_API_PORT } = import.meta.env;
@@ -24,29 +24,23 @@ export const api: ExtendedAxiosInstance = axiosLib.create({
   },
 });
 
-api.graphql = async (
+api.graphql = async <T>(
   query: string,
-  variables: Record<string, any> = {}
-): Promise<object | Array<object>> => {
-  try {
-    console.log("Query: ", query);
-    console.log("Full query: ", queries[query]);
+  variables: GraphqlVariables = {}
+): Promise<T> => {
+  const response: AxiosResponse<GraphqlResult<T>> = await api.post("/", {
+    query: gqlQuery[query],
+    variables,
+  });
 
-    const response = await api.post("/", {
-      query: queries[query],
-      variables,
-    });
-
-    if (response.data.errors) {
-      throw new Error(response.data.errors[0].message);
-    }
-    console.log("Response: ", response.data.data);
-
-    return response.data.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
+  if (response.data.errors) {
+    throw new Error(response.data.errors[0].message);
   }
+
+  if (response.data.data === undefined) {
+    throw new Error("Aucune data");
+  }
+  return response.data.data;
 };
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
