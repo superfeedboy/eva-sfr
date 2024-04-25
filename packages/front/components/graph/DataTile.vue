2e476976f436e0e02caee79801c214173aebccaf
Gilles COGNIN <gillescognin@gmail.com>
2024-04-25 18:27:54 +0200

    Feat: Création d'un composant de titre

---
+++ packages/front/components/graph/DataTile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-24 bg-gradient-to-t from-EVAPurple rounded-xl p-4">
+    <h1 class="text-md bold">{{ title }}</h1>
+    <div class="w-full text-center pt-0">
+      <span class="text-xl md:text-3xl xl:text-5xl w-full">{{ data }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from "vue";
+
+const props = defineProps({
+  title: String,
+  data: [Number, String],
+});
+const { title, data } = toRefs(props);
+</script>
